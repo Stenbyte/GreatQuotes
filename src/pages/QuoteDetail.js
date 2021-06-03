e437a541be55d5fc0dd68558db3a389958f579fa
Stenbyte <sten.scheifel@gmail.com>
2021-06-03 10:45:47 +0300

    nested routing

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Comments from "../components/comments/Comments";
+import { Route, useParams } from "react-router-dom";
 
 const QuoteDetail = () => {
-  return <h1>Quote Detail</h1>;
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
