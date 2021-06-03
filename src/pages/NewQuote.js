9a7e96862a949223a1e34a3b91769d0633c372d8
Stenbyte <sten.scheifel@gmail.com>
2021-06-03 11:58:07 +0300

    programmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
-  const addQuoteHandler = () => {
-    console.log("click");
+  const history = useHistory();
+  const addQuoteHandler = (data) => {
+    console.log(data);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
