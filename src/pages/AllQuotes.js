e01ae0429d02c00d1de9027172624a911d51b2ff
Stenbyte <sten.scheifel@gmail.com>
2021-06-03 11:33:51 +0300

    outputtong data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -3,8 +3,8 @@ import QuoteList from "../components/quotes/QuoteList";
 import QuoteDetail from "./QuoteDetail";
 
 const Dummy = [
-  { id: 1, author: "Sten", text: "Learning React is fun" },
-  { id: 2, author: "Max", text: "Learning React is great" },
+  { id: "q1", author: "Sten", text: "Learning React is fun" },
+  { id: "q2", author: "Max", text: "Learning React is great" },
 ];
 const AllQuotes = () => {
   return <QuoteList quotes={Dummy} />;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from "react";
 import Comments from "../components/comments/Comments";
 import { Route, useParams } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const Dummy = [
+  { id: "q1", author: "Sten", text: "Learning React is fun" },
+  { id: "q2", author: "Max", text: "Learning React is great" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = Dummy.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No data</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
